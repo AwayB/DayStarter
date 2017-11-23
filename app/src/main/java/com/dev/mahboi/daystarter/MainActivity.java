98246e48ca93199f9f5198e5341c56799d8046be
Mahboi <alexandre.weber@epitech.eu>
2017-11-23 12:22:42 +0900

    initial/layout save

---
+++ app/src/main/java/com/dev/mahboi/daystarter/MainActivity.java
@@ -0,0 +1,17 @@
+package com.dev.mahboi.daystarter;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by alex on 13/11/17.
+ */
+
+public class MainActivity extends Activity {
+    Context context = getApplicationContext();
+    View view = new View(context);
+}
